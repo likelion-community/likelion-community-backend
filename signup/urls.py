91919ba8a23a56f54044b0e456262100bd5c5fc6
suncastle023 <badabada0619@naver.com>
2024-10-26 00:39:42 +0900

    feat: 일반로그인+회원가입 구현/fix: 카카오 로그인 닉네임필드 추가

--- signup/urls.py
+++ signup/urls.py
@@ -5,7 +5,8 @@ from social_django import views as social_views
 app_name = 'signup'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),  
+    path('login/', views.login_home_view, name='login_home'),  # 로그인 홈 (카카오, 일반 선택 화면)
+    path('login/custom/', views.custom_login_view, name='login'), 
     path('signup/', views.signup_view, name='signup'),
     path('complete_profile/', views.complete_profile_view, name='complete_profile'), 
     path('delete_user/', views.delete_user, name='delete_user'),
@@ -13,5 +14,7 @@ urlpatterns = [
      # 카카오 소셜 로그인
     path('login/kakao/', social_views.auth, name='begin', kwargs={'backend': 'kakao'}),  
     path('complete/kakao/', social_views.complete, name='complete'), 
+    path('check-password/', views.check_password_view, name='check_password'),
+    
 
 ]
