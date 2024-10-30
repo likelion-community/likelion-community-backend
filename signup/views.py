16c9d231ad80e0f83ff64eb2532dd5438430c784
Ubuntu <ubuntu@ip-172-31-6-7.ap-northeast-2.compute.internal>
2024-10-30 02:21:13 +0000

    fix: view 최신

--- signup/views.py
+++ signup/views.py
@@ -41,12 +41,19 @@ class KakaoLoginAPIView(APIView):
             return redirect('home:mainpage')
         return redirect('signup:login_home')
 
+
+
 class CustomLoginAPIView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        if request.user.is_authenticated or request.user.is_superuser:
-            return redirect('home:mainpage')
+        # 이미 인증된 사용자인 경우
+        if request.user.is_authenticated:
+            # 슈퍼유저 또는 프로필이 완성된 사용자만 메인 페이지로 리디렉트
+            if request.user.is_superuser or request.user.is_profile_complete:
+                return redirect('home:mainpage')
+            else:
+                return redirect('signup:complete_profile')
 
         serializer = CustomLoginSerializer(data=request.data)
         if serializer.is_valid():
@@ -55,8 +62,11 @@ class CustomLoginAPIView(APIView):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
+                # 로그인 후 슈퍼유저는 프로필 검사 없이 통과
                 if user.is_superuser or user.is_profile_complete:
                     return Response({'message': '로그인 성공'}, status=status.HTTP_200_OK)
+                else:
+                    return redirect('signup:complete_profile')
         return Response({'error': '아이디 또는 비밀번호가 잘못되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
