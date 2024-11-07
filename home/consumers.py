07e4b5970a420f0c536eb1bef83b93e20ad125f4
suncastle023 <badabada0619@naver.com>
2024-11-07 22:56:36 +0900

    feat: 게시글에 좋아요 또는 댓글이 추가될 때 알림이 오는 기능 추가

---
+++ home/consumers.py
@@ -0,0 +1,27 @@
+# home/consumers.py
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.group_name = f'notifications_{self.user_id}'
+
+        # 그룹에 추가
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # 그룹에서 제거
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def send_notification(self, event):
+        message = event['message']
+        # 클라이언트에게 메시지 전송
+        await self.send(text_data=json.dumps(message))
