b028a8a988ade2e109e22e420d448bc9d1a0186b
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-08 22:09:50 +0900

    feat: 간단한 헬스 체크 엔드포인트를 추가

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', HomeAPIView.as_view(), name='mainpage'),
+    path('health/', HealthCheckAPIView.as_view(), name='health-check'), 
     path('notifications/', NotificationListView.as_view(), name='notification-list'),
     path('notifications/<int:notification_id>/read/', MarkNotificationAsReadView.as_view(), name='mark-notification-read'),
     path('notifications/<int:notification_id>/delete/', DeleteNotificationView.as_view(), name='delete-notification'),
