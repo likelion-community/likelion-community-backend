c501eda2a878d319ae24da99cfed14d5608b0970
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-10 01:26:42 +0900

    Update serializers.py

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -4,13 +4,15 @@ from .models import Attendance, AttendanceStatus
 class AttendanceSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
     file = serializers.FileField(allow_null=True, required=False)
-    track = serializers.ChoiceField(choices=[('backend', 'Backend'), ('frontend', 'Frontend'), ('design', 'Design')])
+    track = serializers.ChoiceField(choices=Attendance.TRACK_CHOICES)  # 모델의 TRACK_CHOICES와 통일
     auth_code = serializers.CharField(write_only=True, required=True)  # 운영진이 출석 코드를 생성
+    time = serializers.TimeField(required=True) 
 
     class Meta:
         model = Attendance
-        fields = ['id', 'date', 'title', 'auth_code', 'description', 'file', 'track', 'created_by']
-        
+        fields = ['id', 'date', 'time', 'title', 'auth_code', 'description', 'file', 'track', 'created_by']
+
+
 
 class AttendanceStatusSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.name', read_only=True)  # 사용자 이름을 추가로 보여주기
