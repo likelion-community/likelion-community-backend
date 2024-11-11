96a5ece2d55a02ace23a50a9ec906eb8fe7a41be
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-12 00:10:56 +0900

    Update extract_text_and_logo.py

--- extract_text_and_logo.py
+++ extract_text_and_logo.py
@@ -98,7 +98,7 @@ def extract_text(image):
     central_region = image[int(height * 0.15):int(height * 0.85), int(width * 0.15):int(width * 0.85)]
 
     # 중앙 70% 영역에서 큰 스케일로 재시도
-    large_scale = 2.5
+    large_scale = 2.0
     resized_image = cv2.resize(central_region, (int(central_region.shape[1] * large_scale), int(central_region.shape[0] * large_scale)))
     easyocr_results = reader.readtext(resized_image, detail=0)
     print(f"[EasyOCR] 중앙 70% 스케일 {large_scale}에서 검출된 텍스트: {easyocr_results}")
