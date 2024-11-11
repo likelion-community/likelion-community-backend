51b2f8ebdf69b9a2c3c8cc15c4a241d7726cb42c
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-12 02:25:05 +0900

    Update settings.py

--- likelion_community/settings.py
+++ likelion_community/settings.py
@@ -138,11 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # 기본적으로 인증된 사용자에게만 허용
     ],
-    '''
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',  # JSON 포맷을 기본으로 사용
-    ],
-    '''
+
 }
 
 # Authentication Backends
@@ -290,4 +286,3 @@ LOGGING = {
         },
     },
 }
-
