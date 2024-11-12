e70ec549f768a138cd54240ac8ec8d88e550c346
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-13 01:13:27 +0900

    Update settings.py

--- likelion_community/settings.py
+++ likelion_community/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+WHITENOISE_ADD_HEADERS_FUNCTION = None
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
