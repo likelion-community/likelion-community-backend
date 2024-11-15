e31b7d6fabf9007d9fb94f2fbf07ac1aba96edf5
suncastle023 <badabada0619@naver.com>
2024-11-16 03:46:40 +0900

    fix

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -4,18 +4,20 @@ from signup.models import CustomUser
 
 class AttendanceSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
+    creator_name = serializers.CharField(source='created_by.name', read_only=True)
+    creator_term = serializers.IntegerField(source='created_by.membership_term', read_only=True)
+    creator_position = serializers.CharField(source='created_by.is_staff', read_only=True)
     file = serializers.FileField(allow_null=True, required=False)
-    track = serializers.ChoiceField(choices=Attendance.TRACK_CHOICES)  # 모델의 TRACK_CHOICES와 통일
-    auth_code = serializers.CharField(write_only=True, required=True)  # 운영진이 출석 코드를 생성
-    time = serializers.TimeField(required=True) 
-    late_threshold = serializers.IntegerField(required=True)  # 지각 기준 필드
-    absent_threshold = serializers.IntegerField(required=True)  # 결석 기준 필드
-    place = serializers.CharField(required=False, allow_blank=True)  
+    track = serializers.ChoiceField(choices=Attendance.TRACK_CHOICES)
+    auth_code = serializers.CharField(write_only=True, required=True)
+    time = serializers.TimeField(required=True)
+    late_threshold = serializers.IntegerField(required=True)
+    absent_threshold = serializers.IntegerField(required=True)
+    place = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = Attendance
         fields = ['id', 'date', 'time', 'title', 'auth_code', 'description', 'file', 'track', 'created_by', 'creator_name', 'creator_term', 'creator_position', 'late_threshold', 'absent_threshold', 'place']
-
 
 
 class AttendanceStatusSerializer(serializers.ModelSerializer):
