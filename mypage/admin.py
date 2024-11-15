9107993ba4f91c3435844c8d7efc89e5e535fbbd
suncastle023 <139223894+suncastle023@users.noreply.github.com>
2024-11-16 06:01:32 +0900

    Update admin.py

--- mypage/admin.py
+++ mypage/admin.py
@@ -7,6 +7,3 @@ class VerificationAdmin(admin.ModelAdmin):
     list_filter = ('school_status', 'executive_status', 'track')
     search_fields = ('user__username', 'user__email')
     readonly_fields = ('submission_date', 'review_date')
-
-
-admin.site.register(Verification)
