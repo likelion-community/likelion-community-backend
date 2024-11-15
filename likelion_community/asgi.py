5876f965ce310204d2e6105a62cd29512c271ce3
suncastle023 <badabada0619@naver.com>
2024-11-15 18:46:39 +0900

    fix: asgi에서 순서 편집

--- likelion_community/asgi.py
+++ likelion_community/asgi.py
@@ -1,22 +1,23 @@
 # asgi.py
 import os
-import django 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import friend.routing
-import home.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "likelion_community.settings")
 
-# Django 앱 강제 초기화
-django.setup()
+# Django 애플리케이션 초기화
+django_asgi_app = get_asgi_application()
+
+# 라우팅 모듈은 Django 초기화 후에 로드
+import friend.routing
+import home.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # HTTP 요청 처리
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            friend.routing.websocket_urlpatterns + home.routing.websocket_urlpatterns  
+            friend.routing.websocket_urlpatterns + home.routing.websocket_urlpatterns
         )
     ),
 })
